ed59cec00f368e82a3e723e6fdf64328d2b8ef86
samuelckraft <samuelckraft@gmail.com>
2024-08-04 14:44:12 -0400

    update

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from web_socket_server import WebSocketServer, socketio, app
-from flask import render_template
+from flask import render_template, request, json
 
 app = WebSocketServer().create_app()
+message_storage = {}
+
 
 @socketio.on('connect')
 def handle_connect():
@@ -12,13 +14,32 @@ def handle_disconnect():
     print('Client disconnected')
 
 @socketio.on('message')
-def handle_message(message):
-    print(f'Received message: {message}')
-    socketio.emit('message', message) #sends a message from us to the client
+def handle_message(data):
+    try:
+        data = json.loads(data)
+        user = data['user']
+        message = data['message']
+        print(f'Received message from {user}: {message}')
+        
+        if user not in message_storage:
+            message_storage[user] = []
+        message_storage[user].append(message)
+        
+        socketio.emit('message', data)
+    except (KeyError) as e:
+        print(f'Error processing message: {e}')
 
 @app.route('/')
 def index():
     return render_template('WebSocketClient.html')
+
+@app.route('/get_all_messages', methods=['GET'])
+def get_all_messages():
+    user = request.args.get('user')
+    if user in message_storage:
+        print({'messages': message_storage[user]})
+    else:
+        return {'messages': []}
 
 
 
